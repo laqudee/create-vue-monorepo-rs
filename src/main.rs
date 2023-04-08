34d016a4d6ac6c71cf485eabf5fe45203135f896
laqudee <2268678583@qq.com>
2023-04-08 17:01:03 +0800

    feat commit code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use console::Term;
 use dialoguer::{theme::ColorfulTheme, Confirm, Input};
 use std::env;
+use std::fs;
 
-use create_vue_monorepo_rs::{
-    can_skip_emptying, 
-    empty_dir, 
-    is_valid_package_name, 
-    to_valid_package_name,
-};
+// use create_vue_monorepo_rs::{
+//     can_skip_emptying, empty_dir, is_valid_package_name, to_valid_package_name,
+// };
 
 #[derive(Debug)]
 pub struct ConfiguresSelected {
@@ -73,6 +71,11 @@ pub fn dialoguer_work(configures: &mut ConfiguresSelected) -> (String, &Configur
 
     let root = env::current_dir().unwrap().join(project_name.clone());
     println!("root: {:?}", root.display());
+    if fs::metadata(root.clone()).is_ok() {
+        fs::remove_dir_all(root.clone()).unwrap();
+    } else if fs::metadata(root.clone()).is_err() {
+        fs::create_dir(root.clone()).unwrap();
+    }
 
     let config_value: bool = Confirm::with_theme(&theme)
         .with_prompt("Add ESLint for code quality?")
