6b7a380d5c2c5f3e2926ea073a83a9467277cc3c
laqudee <2268678583@qq.com>
2023-04-09 19:51:05 +0800

    feat commit code of render.rs

--- src/utils/render.rs
+++ src/utils/render.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::io::Result;
-use std::io::Write;
+use std::io::{Result, Write};
 use std::path::{Path, PathBuf};
 
 use serde_json::{self, Value};
@@ -8,59 +7,60 @@ use serde_json::{self, Value};
 use super::{deep_merge::merge, sort_dependencied::sort};
 
 pub fn render_template(src: &PathBuf, dest: &PathBuf) -> Result<()> {
-    let stats = fs::metadata(src).unwrap();
-    if stats.is_dir() {
-        if src.file_name() == Some("node_modules".as_ref()) {
-            return Ok(());
+    match fs::metadata(src) {
+        Ok(stats) if stats.is_dir() => {
+            if src.file_name() == Some("node_modules".as_ref()) {
+                return Ok(());
+            }
+
+            fs::create_dir_all(dest)?;
+
+            for entry in fs::read_dir(src)? {
+                let entry = entry?;
+                let src_path = entry.path();
+                let dest_path = dest.join(entry.file_name());
+                render_template(&src_path, &dest_path)?;
+            }
+
+            Ok(())
         }
+        Ok(_) => {
+            // fs::copy()的调用时机，怎么放置，才不会影响package.json的合成
+            fs::copy(src, dest)?;
 
-        // 如果是个目录，则递归渲染其内部的子文件
-        fs::create_dir_all(dest).unwrap();
-        for entry in fs::read_dir(src).unwrap() {
-            let entry = entry.unwrap();
-            let src_path = entry.path();
-            let dest_path = dest.join(entry.file_name());
-            render_template(&src_path, &dest_path)?;
+            let file_name = Path::new(src).file_name().unwrap().to_str().unwrap();
+
+            // package.json的排序与合成都有问题
+            if let ("package.json", Ok(_)) = (file_name, fs::metadata(&dest)) {
+                let existing_contents = fs::read_to_string(&dest).unwrap_or_default();
+                let existing: Value = serde_json::from_str(&existing_contents).unwrap();
+
+                let new_contents = fs::read_to_string(&src).unwrap_or_default();
+                let new_package: Value = serde_json::from_str(&new_contents).unwrap();
+
+                let mut package_json = merge(&existing, &new_package);
+                let pkg = sort(&mut package_json);
+                let pkg = serde_json::to_string_pretty(&pkg)?;
+                let mut file = fs::File::create(&dest)?;
+                file.write_all(pkg.as_bytes())?;
+            }
+
+            if file_name.starts_with('_') {
+                let parent_dir = dest.parent().unwrap();
+                let new_filename = file_name.replacen('_', ".", 1);
+                let dest_path = parent_dir.join(new_filename);
+
+                fs::rename(&dest, &dest_path)?;
+            }
+
+            if let ("_gitignore", Ok(_)) = (file_name, fs::metadata(&dest)) {
+                let existing = fs::read_to_string(dest).unwrap_or_default();
+                let new_gitignore = fs::read_to_string(src).unwrap_or_default();
+                fs::write(dest, existing + "\n" + &new_gitignore)?;
+            }
+
+            Ok(())
         }
-        return Ok(());
+        Err(e) => Err(e),
     }
-
-    // TODO 拷贝文件的时机：如果不在上面拷贝，修改名称的时候报找不到文件；
-    // 放在这里，后面的render_template()调用会覆盖前面的
-    fs::copy(src, dest)?;
-
-    let file_name = Path::new(src).file_name().unwrap().to_str().unwrap();
-
-    // TODO: 处理 package.json 文件 合并时后来的苏醒覆盖了前面的属性
-    if file_name == "package.json" && fs::metadata(&dest).is_ok() {
-        let existing_contents = fs::read_to_string(&dest).unwrap_or_default();
-        let existing: Value = serde_json::from_str(&existing_contents).unwrap();
-
-        let new_contents = fs::read_to_string(&src).unwrap_or_default();
-        let new_package: Value = serde_json::from_str(&new_contents).unwrap();
-
-        let mut package_json = merge(&existing, &new_package);
-        let pkg = sort(&mut package_json);
-        let pkg = serde_json::to_string_pretty(&pkg)?;
-        let mut file = fs::File::create(&dest)?;
-        file.write_all(pkg.as_bytes())?;
-        return Ok(());
-    }
-
-    if file_name.starts_with('_') {
-        let parent_dir = dest.parent().unwrap();
-        let new_filename = file_name.replacen('_', ".", 1);
-        let dest_path = parent_dir.join(new_filename);
-
-        fs::rename(&dest, &dest_path)?;
-    }
-
-    if file_name == "_gitignore" && fs::metadata(dest).is_ok() {
-        let existing = fs::read_to_string(dest).unwrap_or_default();
-        let new_gitignore = fs::read_to_string(src).unwrap_or_default();
-        fs::write(dest, existing + "\n" + &new_gitignore).unwrap();
-        return Ok(());
-    }
-
-    Ok(())
 }
